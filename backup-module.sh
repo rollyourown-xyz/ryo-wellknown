9bc237aeda86edd887a58878891f0871d22996cb
xyzroller <xyzroller@rollyourown.xyz>
2022-01-14 20:49:03 +0100

    Add module backups

---
+++ backup-module.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+helpMessage()
+{
+  echo "backup.sh: Back up module container persistent storage from the host to the control node"
+  echo ""
+  echo "Help: backup-module.sh"
+  echo "Usage: ./backup-module.sh -n hostname"
+  echo "Flags:"
+  echo -e "-n hostname \t\t(Mandatory) Name of the host from which to back up module container persistent storage"
+  echo -e "-h \t\t\tPrint this help message"
+  echo ""
+  exit 1
+}
+
+errorMessage()
+{
+  echo "Invalid option or input variables are missing"
+  echo "Use \"./backup-module.sh -h\" for help"
+  exit 1
+}
+
+while getopts n:h flag
+do
+  case "${flag}" in
+    n) hostname=${OPTARG};;
+    h) helpMessage ;;
+    ?) errorMessage ;;
+  esac
+done
+
+if [ -z "$hostname" ]
+then
+  errorMessage
+fi
+
+# Get Module ID from configuration file
+MODULE_ID="$(yq eval '.module_id' "$SCRIPT_DIR"/configuration/configuration.yml)"
+
+
+# Info: no persistent storage
+#############################
+
+echo ""
+echo ""$MODULE_ID" on "$hostname" has no persistent storage to back up"
