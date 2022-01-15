afb7057ee002bbc676dbcbdd17b626cc8463d466
xyzroller <xyzroller@rollyourown.xyz>
2022-01-15 18:44:01 +0100

    Always run host-setup playbook

--- scripts-module/host-setup.sh
+++ scripts-module/host-setup.sh
@@ -36,17 +36,11 @@ then
    errorMessage
 fi
 
-# Get Module ID from configuration file
-MODULE_ID="$(yq eval '.module_id' "$SCRIPT_DIR"/../configuration/configuration.yml)"
 
-# Module-specific host setup for "$MODULE_ID"
-if [ -f ""$SCRIPT_DIR"/../configuration/"$hostname"_playbooks_executed" ]
-then
-   echo "Host setup for "$MODULE_ID" module has already been done on "$hostname""
-   echo ""
-else
-   echo "Executing module-specific host setup playbooks for "$MODULE_ID" on "$hostname""
-   echo ""
-   ansible-playbook -i "$SCRIPT_DIR"/../../ryo-host/configuration/inventory_"$hostname" "$SCRIPT_DIR"/../host-setup/main.yml --extra-vars "host_id="$hostname""
-   touch "$SCRIPT_DIR"/../configuration/"$hostname"_playbooks_executed
-fi
+# Run host setup playbooks for module
+echo ""
+echo "Running module-specific host setup on "$hostname""
+echo ""
+echo "Executing module-specific host setup on "$hostname""
+ansible-playbook -i "$SCRIPT_DIR"/../../ryo-host/configuration/inventory_"$hostname" "$SCRIPT_DIR"/../host-setup/main.yml --extra-vars "host_id="$hostname""
+echo "Completed"
