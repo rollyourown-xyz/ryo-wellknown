0043e09e0a286e311ab979bd53200c1a4d28af30
xyzroller <xyzroller@rollyourown.xyz>
2022-04-17 21:07:01 +0200

    Add IPv6 configuration

--- module-deployment/00-variables.tf
+++ module-deployment/00-variables.tf
@@ -21,16 +21,9 @@ variable "image_version" {
 # Configuration file paths
 locals {
   module_configuration = "${abspath(path.root)}/../configuration/configuration.yml"
-  host_configuration   = join("", ["${abspath(path.root)}/../../ryo-host/configuration/configuration_", var.host_id, ".yml" ])
 }
 
 # Basic module variables
 locals {
-  module_id                   = yamldecode(file(local.module_configuration))["module_id"]
-}
-
-# LXD variables
-locals {
-  lxd_host_control_ipv4_address  = yamldecode(file(local.host_configuration))["host_control_ip"]
-  lxd_host_network_part         = yamldecode(file(local.host_configuration))["lxd_host_network_part"]
+  module_id = yamldecode(file(local.module_configuration))["module_id"]
 }
